144003ff43bd1de82e21aeb6fbbe353e956594fd
jeddaa <fathiato@gmail.com>
2023-02-23 07:24:33 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,19 +5,19 @@
 def validUTF8(data):
     """:type data: List[int]
     rtype: bool"""
-    count = 0
-    for c in data:
-        if count == 0:
-            if (c >> 5) == 0b110 or c >> 5 == 0b1110:
-                count = 1
-            elif (c >> 4) == 0b1110:
-                count = 2
-            elif (c >> 3) == 0b11110:
-                count = 3
-            elif (c >> 7):
+    numOfByte = 0
+    for x in data:
+        if numOfByte == 0:
+            if (x >> 5) == 0b110 or x >> 5 == 0b1110:
+                numOfByte = 1
+            elif (x >> 4) == 0b1110:
+                numOfByte = 2
+            elif (x >> 3) == 0b11110:
+                numOfByte = 3
+            elif (x >> 7):
                 return False
         else:
-            if (c >> 6) != 0b10:
+            if (x >> 6) != 0b10:
                 return False
-            count -= 1
-    return count == 0
+            numOfByte -= 1
+    return numOfByte == 0
