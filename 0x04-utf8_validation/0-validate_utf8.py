ccd5ede854d5d0169375720fe2194b574807a2dc
jeddaa <fathiato@gmail.com>
2023-02-23 07:13:37 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-""" utf-8 validataion module """
-
-
+""" utf-8 validataion module
 def validUTF8(data):
-    """
     :type data: List[int]
-    :rtype: bool
-    """
+    rtype: bool
     count = 0
     for c in data:
         if count == 0:
@@ -23,3 +19,49 @@ def validUTF8(data):
                 return False
             count -= 1
     return count == 0
+    """
+
+from itertools import takewhile
+
+
+NUMBER_OF_BITS_PER_BLOCK = 8
+MAX_NUMBER_OF_ONES = 4
+
+
+def to_bits(bytes):
+    """ func to convert list of integers to bit"""
+    for byte in bytes:
+        num = []
+        exp = 1 << NUMBER_OF_BITS_PER_BLOCK
+        while exp:
+            exp >>= 1
+            num.append(bool(byte & exp))
+        yield num
+
+
+def validUTF8(data):
+    """
+    :type data: List[int]
+    :rtype: bool
+    """
+    bits = to_bits(data)
+    for byte in bits:
+        # single byte char
+        if byte[0] == 0:
+            continue
+
+        # multi-byte character
+        amount = sum(takewhile(bool, byte))
+        if amount <= 1:
+            return False
+        if amount >= MAX_NUMBER_OF_ONES:
+            return False
+
+        for _ in range(amount - 1):
+            try:
+                byte = next(bits)
+            except StopIteration:
+                return False
+            if byte[0:2] != [1, 0]:
+                return False
+    return True
